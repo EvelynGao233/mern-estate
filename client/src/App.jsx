75d2d80542fc1a4cab73de488a7fba573e5300e6
Evelyn Gao <ygao@smith.edu>
2023-11-14 16:36:22 -0500

    update the header and private the profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './components/header'
+import PrivateRoute from "./components/PrivateRoute"
 function App() {
   return <BrowserRouter>
   <Header />
@@ -14,7 +15,9 @@ function App() {
     <Route path="/sign-in" element={<SignIn />}/>
     <Route path="/sign-up" element={<SignUp />}/>
     <Route path="/about" element={<About />}/>
-    <Route path="/profile" element={<Profile />}/>
+    <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
   </Routes>
   </BrowserRouter>
 }
